931449d9c3c25e263f28735fc1e4a0cd3555b32e
Gary J. Paderanga <gamae144@yahoo.com>
2023-07-28 19:07:47 +0300

    Firts React Activity Assignment

---
+++ src/layouts/Header.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+
+ return(
+
+    <header> 
+           <Link to="/"> </Link> 
+           <Link to="/home"> Home </Link> 
+           <Link to="/about"> About </Link> 
+           <Link to="/contact"> Contact </Link> 
+           <Link to="/blog"> Blog </Link> 
+    </header> 
+
+   )
+  
+
+}
+
+export default Header;
---
+++ src/pages/Contact.js
@@ -0,0 +1,70 @@
+
+const Contact = () => {
+
+    return(
+
+        // <div className="Contact">
+            <main> 
+
+                <div className="notification-item">
+                    <div className="noti-icon"><i class="fa-solid fa-earth-americas"></i></div>
+                   
+                <div className="noti-col">
+                    <div className="noti-details">
+                            <h3>Contact Us</h3>
+                            
+                            <div className="feedback-from">
+
+                                <div className="feedback-from-div">
+                                    <h3>HELP US TO IMPROVE</h3>
+                                    <p>Do you have a Inquiries or suggestions?
+                                        Let us know in the field below.
+                                    </p>
+                                </div>
+
+                                <div className="questionnaire"></div>
+                                    <h4>How was your experience?</h4>
+
+                                    
+                                        {/* </div></div><input type="radio" id="radioButton1" name="options"> */}
+                                        {/* <label for="radioButton1" class="icon"> */}
+                                            {/* <i className="fa-regular fa-face-smile"></i> */}
+                                        {/* </label> */}
+                                    
+                                        {/* <input type="radio" id="radioButton2" name="options"> */}
+                                        {/* <label for="radioButton2" class="icon"> */}
+                                            {/* <i className="fa-regular fa-face-frown"></i> */}
+                                        {/* </label> */}
+                                    
+                                        {/* <input type="radio" id="radioButton3" name="options"> */}
+                                        {/* <label for="radioButton3" class="icon"> */}
+                                            {/* <i className="fa-regular fa-face-meh"></i> */}
+                                        {/* </label> */}
+                                
+                                        {/* <input type="radio" name="suggestion" id=""> */}
+                                        {/* <textarea name="" id="" cols="30" rows="7" placeholder="Describe your experience here ..."></textarea> */}
+
+                                        {/* <input type="radio" id="radioButton4" name="category"> */}
+                                        {/* <label for="radioButton4" class="text"> Inquiry </label> */}
+
+                                        {/* <input type="radio" id="radioButton5" name="category"> */}
+                                        {/* <label for="radioButton5" class="text"> Suggestion </label> */}
+
+                                        {/* <input type="radio" id="radioButton6" name="category"> */}
+                                        {/* <label for="radioButton6" class="text"> Others </label> */}
+
+                                        <button className="btn-blue">SUBMIT</button>
+                                
+                            </div>
+                        
+                    </div>        
+                </div>
+                </div>
+        
+            {/* </div> */}
+        </main>
+    )
+
+}
+
+export default Contact
